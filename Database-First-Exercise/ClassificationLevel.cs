2cf98795b3990e58bd297a6e12e38aca16dc6f8e
harsh_git <guptaharsh686@gmail.com>
2023-07-10 00:09:09 +0530

    Implemented the Exercise

---
+++ Database-First-Exercise/ClassificationLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_First_Exercise
+{
+    public enum ClassificationLevel : byte
+    {
+        Silver = 1,
+        Gold,
+        Platinum
+    }
+}
---
+++ Database-First-Exercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_First_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new VidzyDbContext();
+
+            context.AddVideo("Video 15", DateTime.Now, "Comedy",(byte)ClassificationLevel.Platinum);
+            context.AddVideo("Video 16", DateTime.Now, "Action", (byte)ClassificationLevel.Platinum);
+
+
+
+        }
+    }
+}
