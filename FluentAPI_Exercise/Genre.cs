819a710667937042b0409ef270d4c8d9e2067def
harsh_git <guptaharsh686@gmail.com>
2023-07-19 23:30:14 +0530

    Added code forst exercise code to continue fluent API Exercise

---
+++ FluentAPI_Exercise/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_First_Exercise
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IList<Video> Videos { get; set; }
+    }
+}
