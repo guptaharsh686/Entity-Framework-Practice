d74c5683f58c62bddf9d634f74064dcebbeb7279
harsh_git <guptaharsh686@gmail.com>
2023-07-20 00:52:18 +0530

    example of using linq to query a database

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -1,10 +1,37 @@
-﻿
+﻿using System.Linq;
 namespace Queries
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var context = new PlutoContext();
+
+            //get data using linq syntax
+
+            var query =
+                from c in context.Courses
+                where c.Name.Contains("c#")
+                orderby c.Name
+                select c;
+
+
+            foreach (var cource in query)
+            {
+                System.Console.WriteLine(cource.Name);
+            }
+
+
+            //get data using extension methods
+
+            var courses = context.Courses
+                                  .Where(c => c.Name.Contains("c#"))
+                                  .OrderBy(c => c.Name);
+
+            foreach (var cource in query)
+            {
+                System.Console.WriteLine(cource.Name);
+            }
 
         }
     }
