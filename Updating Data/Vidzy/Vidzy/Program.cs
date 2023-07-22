75a0fe179601c39cc99c96c96edb07ff05a93692
harsh_git <guptaharsh686@gmail.com>
2023-07-23 02:44:13 +0530

    implemented the exercise my way

--- Updating Data/Vidzy/Vidzy/Program.cs
+++ Updating Data/Vidzy/Vidzy/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Linq;
+using System.Data.Entity;
 
 namespace Vidzy
 {
@@ -7,7 +8,120 @@ namespace Vidzy
     {
         static void Main(string[] args)
         {
-            AddVideo();
+            //AddVideo();
+            //AddTags();
+            //AddTags();
+
+            //AddTagsToVideo();
+            //AddTagsToVideo();
+
+            //RemoveComedyTag();
+
+            //RemoveVideo();
+
+            RemoveGenre();
+
+
+        }
+
+        static void RemoveGenre()
+        {
+            using(var context = new VidzyContext())
+            {
+                var genreAvailable = context.Genres.Select(a => a.Id).ToList().Contains(2);
+                if (genreAvailable)
+                {
+                    var genre = context.Genres.Include(a => a.Videos).Single(a => a.Id == 2);
+
+                    //first remove all the films related to that genre
+                    context.Videos.RemoveRange(genre.Videos);
+                    //then remove genre
+                    context.Genres.Remove(genre);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        static void RemoveVideo()
+        {
+            using(var context = new VidzyContext())
+            {
+                var videoavailable = context.Videos.Select(a => a.Id).Contains(1);
+
+                if(videoavailable)
+                {
+                    var video = context.Videos.Single(a => a.Id == 1);
+                    context.Videos.Remove(video);
+                }
+
+                context.SaveChanges();
+
+            }
+        }
+
+        static void RemoveComedyTag()
+        {
+            using(var context = new VidzyContext())
+            {
+                var video = context.Videos.Single(a => a.Id == 1);
+
+                if(video.Tags.Any(a => a.Name == "comedy"))
+                {
+                    video.Tags.Remove(video.Tags.Single(a => a.Name == "comedy"));
+                }
+
+                context.SaveChanges();
+            }
+        }
+        static void AddTagsToVideo()
+        {
+            using(var context = new VidzyContext())
+            {
+                var video = context.Videos.Single(a => a.Id == 1);
+                var classicTag = context.Tags.Single(a => a.Name == "classics");
+                var dramaTag = context.Tags.Single(a => a.Name == "drama");
+                var comedyTag = new Tag
+                {
+                    Name = "comedy"
+                };
+                
+
+
+                video.Tags.Add(classicTag);
+                video.Tags.Add(dramaTag);
+
+                if (!context.Tags.Select(a => a.Name).ToList().Contains("comedy"))
+                {
+                    context.Tags.Add(comedyTag);
+                    video.Tags.Add(comedyTag);
+                }
+
+
+                context.SaveChanges();
+            }
+        }
+
+        static void AddTags()
+        {
+            using(var context = new VidzyContext())
+            {
+                var classicTag = new Tag
+                {
+                    Name = "classics",
+                };
+
+                var dramaTag = new Tag
+                {
+                    Name = "drama",
+                };
+
+                if(!context.Tags.Select(a => a.Name).ToList().Contains(classicTag.Name))
+                    context.Tags.Add(classicTag);
+                if (!context.Tags.Select(a => a.Name).ToList().Contains(dramaTag.Name))
+                     context.Tags.Add(dramaTag);
+
+                context.SaveChanges();
+            }
         }
 
         static void AddVideo()
