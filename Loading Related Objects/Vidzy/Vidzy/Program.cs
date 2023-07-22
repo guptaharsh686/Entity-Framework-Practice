0d2a3d75277aa50e1ca179238934ee3701231b44
harsh_git <guptaharsh686@gmail.com>
2023-07-22 15:10:45 +0530

    Initial commit with Lazy loading not working

---
+++ Loading Related Objects/Vidzy/Vidzy/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Vidzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new VidzyContext();
+
+            var videos = context.Videos;
+
+            foreach (var vid in videos)
+            {
+                System.Console.WriteLine($"Name = {vid.Name} : Genre = {vid.Genre.Name}");
+            }
+        }
+    }
+}
