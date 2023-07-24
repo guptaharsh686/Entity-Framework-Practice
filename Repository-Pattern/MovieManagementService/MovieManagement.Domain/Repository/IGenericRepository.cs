49837b85196fe59a350f8a2d762f4baef980b1b8
harsh_git <guptaharsh686@gmail.com>
2023-07-25 00:04:32 +0530

    Added template for Repository Pattern using IRepository

---
+++ Repository-Pattern/MovieManagementService/MovieManagement.Domain/Repository/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManagement.Domain.Repository
+{
+    public interface IGenericRepository<TClass> where TClass : class
+    {
+        TClass GetById(int id);
+
+        IEnumerable<TClass> GetAll();
+
+        IEnumerable<TClass> Find(Expression<Func<TClass, bool>> predicate);
+
+        void Add(TClass entity);
+
+        void AddRange(IEnumerable<TClass> entities);
+
+        void Remove(TClass entity);
+        void RemoveRange(IEnumerable<TClass> entities);
+    }
+}
