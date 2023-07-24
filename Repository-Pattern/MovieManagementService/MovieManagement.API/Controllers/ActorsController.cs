0721de5cd88f19583248fedebc9d7b3d505b4ef4
harsh_git <guptaharsh686@gmail.com>
2023-07-25 00:50:59 +0530

    Created Controller to get actors

---
+++ Repository-Pattern/MovieManagementService/MovieManagement.API/Controllers/ActorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieManagement.Domain.Repository;
+
+namespace MovieManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActorsController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public ActorsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var actorsFromRepo = _unitOfWork.Actor.GetAll();
+            return Ok(actorsFromRepo);
+        }
+    }
+}
