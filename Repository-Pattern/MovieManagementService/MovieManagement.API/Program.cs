73eddb5346057624c6df5a4d660984dd123c27e3
harsh_git <guptaharsh686@gmail.com>
2023-07-24 23:53:35 +0530

    Used Entity Framework to create database

--- Repository-Pattern/MovieManagementService/MovieManagement.API/Program.cs
+++ Repository-Pattern/MovieManagementService/MovieManagement.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MovieManagement.DataAccess.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//add Entity Framework Core
+builder.Services.AddDbContext<MovieManagementDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MovieConnection")));
 
 var app = builder.Build();
 
---
+++ Repository-Pattern/MovieManagementService/MovieManagement.DataAccess/Context/MovieManagementDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManagement.DataAccess.Context
+{
+    public class MovieManagementDbContext : DbContext
+    {
+        public MovieManagementDbContext(DbContextOptions<MovieManagementDbContext> options) : base(options) { }
+
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Biography> Biographies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor>()
+                .HasData(
+                    new Actor { Id = 1 , FirstName = "Chuck" , LastName = "Norris"},
+                    new Actor { Id = 2, FirstName = "Jane", LastName = "Doe" },
+                    new Actor { Id = 3, FirstName = "Van", LastName = "Damme" }
+                );
+
+
+            modelBuilder.Entity<Movie>()
+                .HasData(
+                    new Movie { Id = 1, Name = "Wakanda Forever", Description = "adadascasc", ActorId = 1},
+                    new Movie { Id = 2, Name = "Wakanda Forever", Description = "adadascasc", ActorId = 2 },
+                    new Movie { Id = 3, Name = "SpiderMan", Description = "adadascasc", ActorId = 1 },
+                    new Movie { Id = 4, Name = "Matrix", Description = "adadascasc", ActorId = 3 }
+
+                );
+        }
+
+    }
+}
